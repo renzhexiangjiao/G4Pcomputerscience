0b5b6c7894b49b6ac248f63d1f0541d7c008cf4b
renzhexiangjiao <franeksowul@gmail.com>
2018-05-03 00:11:22 +0200

    specimen

---
+++ G4Pcs/Player.Designer.cs
@@ -0,0 +1,51 @@
+﻿namespace G4Pcs
+{
+    partial class Player
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 561);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MinimizeBox = false;
+            this.Name = "Player";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Player";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ G4Pcs/Specimen.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G4Pcs
+{
+    class Specimen
+    {
+        public List<Joint> jointList = new List<Joint>();
+        public List<Bone> boneList = new List<Bone>();
+
+        private Point position;
+        private double score;
+
+        public Specimen()
+        {
+            foreach(Joint joint in jointList)
+            {
+                position.X += joint.getPosition().X;
+                position.Y += joint.getPosition().Y;
+            }
+            position.X /= jointList.Count;
+            position.Y /= jointList.Count;
+
+            score = position.X;
+        }
+
+        public Point getPosition() => position;
+    }
+}
